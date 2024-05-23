bdb1680d33d257e1c88dffb7d9178ff9b7cc42e4
Sergio Arizmendi <sergioarizmendi@rtmdist.com>
2024-05-23 01:22:00 -0600

    fix: bug with
    Removed call to "Alpha" on startup. Without "nvim-tree" opening on startup, "Alpha" opens automatically as a dashboard starting screen.

--- lua/core/startup_comands.lua
+++ lua/core/startup_comands.lua
@@ -2,4 +2,3 @@
 -- Autocommand functions.
 -----------------------------------------------------------
 vim.cmd(":cd $USERPROFILE")         -- Start at the user's directory.
-vim.cmd(":Alpha")                   -- Open Alpha on startup.
