0ea3cf572f46c99f646eb4fea2f4dd697b7c05a3
SergioAFRTM <sergioarizmen@gmail.com>
2026-04-08 01:48:02 -0600

    feat: created custom mappings function util

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,13 +2,8 @@
 -- Keymap Definitions
 -----------------------------------------------------------
 
-local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+-- Get the custom utility map function.
+map = require("core.utils.map")
 
 -- Change leader to a comma.
 vim.g.mapleader = ","
@@ -19,60 +14,59 @@ vim.g.maplocalleader = ","
 -----------------------------------------------------------
 
 -- Move in insert mode.
-map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
+map("i", "<C-b>", "<ESC>^i", "move beginning of line")
+map("i", "<C-e>", "<End>", "move end of line")
+map("i", "<C-h>", "<Left>", "move left")
+map("i", "<C-l>", "<Right>", "move right")
+map("i", "<C-j>", "<Down>", "move down")
+map("i", "<C-k>", "<Up>", "move up")
 
 -- Clear search highlighting with <leader> and c.
-map("n", "<leader>c", "<cmd>nohl<CR>", { desc = "clear highlighting" })
+map("n", "<leader>c", "<cmd>nohl<CR>", "clear highlighting")
 
 -- Move around splits using Ctrl + {h,j,k,l}.
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+map("n", "<C-h>", "<C-w>h", "switch window left")
+map("n", "<C-l>", "<C-w>l", "switch window right")
+map("n", "<C-j>", "<C-w>j", "switch window down")
+map("n", "<C-k>", "<C-w>k", "switch window up")
 
 -- Hole file commands.
-map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
-map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+map("n", "<C-s>", "<cmd>w<CR>", "general save file")
+map("n", "<C-c>", "<cmd>%y+<CR>", "general copy whole file")
 
 -- Create new line in normal mode.
-map("n", "<CR>", "o<Esc>", { desc = "create new line" })
+map("n", "<CR>", "o<Esc>", "create new line")
 
 -- Change line number settings.
-map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
-map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
-map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
+map("n", "<leader>n", "<cmd>set nu!<CR>", "toggle line number")
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", "toggle relative number")
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", "toggle nvcheatsheet")
 
 -- Keep copied content as is after paste.
-map("x", "<leader>p", "\"_dP", { desc = "copy and keep content" })
+map("x", "<leader>p", "\"_dP", "copy and keep content")
 
 -- Adds an automatic replace functionality.
 map("n", "<leader>s", [[\<cmd\>%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "automatic replace" })
+    "automatic replace")
 
 -- Resize windows.
-map("n", "<C-up>", "<cmd>resize -2<cr>", { desc = "reduce vertial window size" })
-map("n", "<C-down>", "<cmd>resize +2<cr>", { desc = "increase vertial window size" })
-map("n", "<C-left>", "<cmd>vertical resize -2<cr>", { desc = "reduce horizontal window size" })
-map("n", "<C-right>", "<cmd>vertical resize +2<CR>", { desc = "increase horizontal window size" })
+map("n", "<C-up>", "<cmd>resize -2<cr>", "reduce vertial window size")
+map("n", "<C-down>", "<cmd>resize +2<cr>", "increase vertial window size")
+map("n", "<C-left>", "<cmd>vertical resize -2<cr>", "reduce horizontal window size")
+map("n", "<C-right>", "<cmd>vertical resize +2<CR>", "increase horizontal window size")
 
 -- Display error diagnostics on <leader> and e.
 map("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float(0, {scope=\"line\"})<CR>",
-    { desc = "display [e]rror diagnostics" })
+    "display [e]rror diagnostics")
 
 -- Display all diagnostic information as list.
-map('n', '<leader>q', "<cmd>lua vim.diagnostic.setloclist()<CR>", { desc = 'open diagnostic [q]uickfix list' })
+map("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", "open diagnostic [q]uickfix list")
 
 -- Move lines around.
 -- NOTE: The explicit use of ":" rather than "<cmd>" is due so Nvim doesn't try to populate the "<>" as visual marks.
-map("v", "J", "<Esc><cmd>m '>+1<CR>gv=gv", { desc = "move line down" })
-map("v", "K", "<Esc><cmd>m '<-2<CR>gv=gv", { desc = "move line up" })
+map("v", "J", "<Esc><cmd>m '>+1<CR>gv=gv", "move line down")
+map("v", "K", "<Esc><cmd>m '<-2<CR>gv=gv", "move line up")
 
 -- Terminal mappings.
-map("n", "<C-t>", "<cmd>Term<CR>", { desc = "open terminal" })
-map("t", "<Esc>", "<C-\\><C-n>", { desc = "close terminal" })
-
+map("n", "<C-t>", "<cmd>Term<CR>", "open terminal")
+map("t", "<Esc>", "<C-\\><C-n>", "close terminal")
---
+++ lua/core/utils/map.lua
@@ -0,0 +1,16 @@
+-- Maps keypas with forced silen noremap.
+-- @param mode Vim mode.
+-- @param mapFrom Expected input to map from.
+-- @param mapTo Output mapping.
+-- @param description Mapping description, automatically mapped to options.desc.
+-- @param options Options passed to the mapping, gets noremap and silent added.
+local function map(mode, mapFrom, mapTo, description, options)
+    description = description or ''
+    local base_options = { noremap = true, silent = true, desc = description }
+    if options then
+        base_options = vim.tbl_extend("force", base_options, options)
+    end
+    vim.api.nvim_set_keymap(mode, mapFrom, mapTo, base_options)
+end
+
+return map
